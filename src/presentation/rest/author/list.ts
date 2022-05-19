e86563afda740d65e22eb970351528c3e663bae1
Lucas Santos <lhs.santoss@gmail.com>
2022-05-20 01:31:42 +0200

    fix: remove unused parameter
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/presentation/rest/author/list.ts
+++ src/presentation/rest/author/list.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { AuthorService } from '../../../services'
 
-export const list = (authorService: AuthorService) => async (req: Request, res: Response) => {
+export const list = (authorService: AuthorService) => async (_: Request, res: Response) => {
   const authors = await authorService.listAll()
   res.status(200).json(authors)
 }
