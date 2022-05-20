2f99674e942db48b630be5a5be4c994111519e63
Lucas Santos <lhs.santoss@gmail.com>
2022-05-20 20:03:12 +0200

    feat: add body parser
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- src/presentation/rest/index.ts
+++ src/presentation/rest/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import { Config } from '../..'
 import { authorRoutesFactory } from './author'
 import { bookRoutesFactory } from './book'
+import bodyParser from 'body-parser'
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const restLayer = (config: Config) => {
+  app.use(bodyParser.json())
   app.use('/books', bookRoutesFactory(config.services))
   app.use('/authors', authorRoutesFactory(config.services))
   app.listen(config.port, () => console.log(`Listening on port ${PORT}`))
